4b9cc87285bc458f315fff61773954d674c20e2b
MAMY Rodrigue <rodrigue.mamy@etu.univ-amu.fr>
2026-03-26 17:15:08 +0100

    Ajout des tests pour la gestion des contraintes

--- tests/testthat/test-grid.R
+++ tests/testthat/test-grid.R
@@ -7,3 +7,13 @@ test_that("Création de grille fonctionne", {
   expect_equal(ncol(grid$constraints), 5)
   expect_equal(length(grid$edges), 0)
 })
+
+test_that("Ajout de contraintes fonctionne", {
+  grid <- create_grid(3, 3)
+  grid$add_constraint(1, 1, 2)
+  grid$add_constraint(2, 2, 3)
+
+  expect_equal(grid$constraints[1, 1], 2)
+  expect_equal(grid$constraints[2, 2], 3)
+  expect_true(is.na(grid$constraints[1, 2]))
+})
